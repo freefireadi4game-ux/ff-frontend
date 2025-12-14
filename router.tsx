b781af38ae11b631759ce130d89f91bd76f5313e
freefireadi4game-ux <freefireadi4game@gmail.com>
2025-12-14 14:10:26 +0530

    Update router.tsx

--- router.tsx
+++ router.tsx
@@ -1,14 +1,18 @@
-import { createBrowserRouter } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Admin from "./pages/Admin";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UploadBox from "./components/UploadBox";
+import Leaderboard from "./components/Leaderboard";
+import AdminToggle from "./components/AdminToggle";
+import Loading from "./components/Loading";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Dashboard />,
-  },
-  {
-    path: "/admin",
-    element: <Admin />,
-  },
-]);
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UploadBox />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/admin" element={<AdminToggle />} />
+        <Route path="/loading" element={<Loading />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
